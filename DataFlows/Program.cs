522ebab6b32a31ced470796b38c1141ffb731e4d
刘伟超 <33384844+lwc1st@users.noreply.github.com>
2022-09-30 08:39:06 +0800

    Dataflow

---
+++ DataFlows/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Tasks.Dataflow;
+
+var actionBlock=new ActionBlock<int>((msg) =>
+{
+    Console.WriteLine("ActionBlock receive:"+msg);
+});
+for (int i = 0; i < 100; i++)
+{
+    await actionBlock.SendAsync(i);
+}
+
+
+var bufferBlock=new BufferBlock<int>();
+for (int i = 0; i < 100; i++)
+{
+    await bufferBlock.SendAsync(i);
+}
+
+while (true)
+{
+   var msg=await bufferBlock.ReceiveAsync();
+   Console.WriteLine("BufferBlock receive:"+msg);
+}
+Console.ReadKey();
